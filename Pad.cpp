cf774431e52372fe1bde6a6641af9483bbc08bc8
西村 龍太 <fko2247017@stu.o-hara.ac.jp>
2022-10-07 14:00:31 +0900

    a

---
+++ Pad.cpp
@@ -0,0 +1,20 @@
+#include"Pad.h"
+
+namespace Pad
+{
+	// ‰Ÿ‚µ‰º‚°”»’è
+	bool isPress(int button)
+	{
+		return false;
+	}
+	// ƒgƒŠƒK[”»’è
+	bool isTrigger(int button)
+	{
+		return false;
+	}
+	// —£‚µ‚½”»’è
+	bool isRelase(int button)
+	{
+		return false;
+	}
+}
