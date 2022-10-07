6d2047c645f6fa2ca730526db1f2e88c48521597
西村 龍太 <fko2247017@stu.o-hara.ac.jp>
2022-10-07 13:58:28 +0900

    a

--- SelectMenu.cpp
+++ SelectMenu.cpp
@@ -1,4 +1,11 @@
 #include"SelectMenu.h"
+#include"Dxlib.h"
+
+namespace
+{
+	// メニュー項目の間隔
+	constexpr int kMenuItemInterval = 32;
+}
 
 // =======================================
 // SelectMenu::Item
@@ -14,9 +21,64 @@ SelectMenu::Item::~Item()
 
 }
 
+void SelectMenu::Item::draw(int x, int y)
+{
+	DrawString(x, y, m_text, GetColor(255, 255, 255));
+}
+
+int SelectMenu::Item::getTextWidth()
+{
+	return GetDrawStringWidth(m_text, strlen(m_text));
+}
+
 void SelectMenu::Item::setText(const char* text)
 {
 	m_text = text;
+}
+
+// =======================================
+// SelectMenu::Cursor
+// =======================================
+SelectMenu::Cursor::Cursor()
+{
+	m_selectIndex = 0;
+	m_itemNum = 4;
+}
+
+SelectMenu::Cursor::~Cursor()
+{
+
+}
+
+void SelectMenu::Cursor::update()
+{
+	int padState = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+		if (padState & PAD_INPUT_UP)
+		{
+			m_selectIndex--;
+			// 上下ループ
+			if (m_selectIndex < 0)
+			{
+				m_selectIndex = m_itemNum - 1;
+			}
+		}
+		if (padState & PAD_INPUT_DOWN)
+		{
+			m_selectIndex++;
+			// 上下ループ
+			if (m_selectIndex > m_itemNum - 1)
+			{
+				m_selectIndex = 0;
+			}
+		}
+}
+
+void SelectMenu::Cursor::draw()
+{
+	int posX = m_menuPos.x;
+	int posY = m_menuPos.y + kMenuItemInterval * m_selectIndex;
+
+	DrawBox(posX, posY, posX + m_size.x, posY + m_size.y, GetColor(255, 0, 0), false);
 }
 
 // =======================================
@@ -35,20 +97,77 @@ SelectMenu::~SelectMenu()
 
 void SelectMenu::init()
 {
-
+	
 }
 
 void SelectMenu::end()
 {
-
+	for (auto& pItem : m_pItem)
+	{
+		delete pItem;
+	}
+	m_pItem.clear();
 }
 
 void SelectMenu::update()
 {
-
+	m_cursor.update();
 }
 
 void SelectMenu::draw()
 {
+	int width = 0;
+	int height = kMenuItemInterval * m_pItem.size();
 
+	
+	DrawBox(m_pos.x, m_pos.y, 128, 128, GetColor(64, 64, 64), true);
+	for (int i = 0; i < m_pItem.size(); i++)
+	{
+		m_pItem[i]->draw(m_pos.x,m_pos.y + i * kMenuItemInterval);
+	}
+	m_cursor.draw();
+}
+
+void SelectMenu::setupCursor()
+{
+	m_cursor.setMenuPos(m_pos);
+	m_cursor.setSize(Vec2(getWindouWidth(),kMenuItemInterval));
+	
+}
+
+void SelectMenu::setPos(float x, float y)
+{
+	setPos(Vec2(x, y));
+}
+
+void SelectMenu::setPos(Vec2 pos)
+{
+	m_pos = pos;
+}
+
+void SelectMenu::addItem(const char* text)
+{
+	Item* pItem = new Item;
+	pItem->setText(text);
+	m_pItem.push_back(pItem);
+}
+
+int SelectMenu::getWindouWidth()
+{
+	int width = 0;
+
+	// 一番横幅の広いメニュー項目の幅をウインドウサイズにする
+	for (auto& pItem : m_pItem)
+	{
+		if (width < pItem->getTextWidth())
+		{
+			width = pItem->getTextWidth();
+		}
+	}
+	return width;
+}
+
+int SelectMenu::getWindouHeight()
+{
+	return kMenuItemInterval * m_pItem.size();
 }
--- SelectMenu.h
+++ SelectMenu.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include<vector>
+#include"Vec2.h"
+
 class SelectMenu
 {
 public:
@@ -15,6 +18,16 @@ public:
 	// 描画
 	void draw();
 
+	// カーソルの初期化
+	void setupCursor();
+
+	int getWindouWidth();
+	int getWindouHeight();
+	// 表示位置の設定
+	void setPos(float x,float y);
+	void setPos(Vec2 vec);
+	// メニュー項目の追加
+	void addItem(const char* text);
 private:
 	class Item
 	{
@@ -22,12 +35,49 @@ private:
 		Item();
 		virtual ~Item();
 
+		// メニュー項目の表示
+		void draw(int x, int y);
+
+		// 表示する文字列を設定
 		void setText(const char* text);
+		// 文字列の幅取得
+		int getTextWidth();
 	private:
 		// 表示する文字列
 		const char* m_text;
 	};
+	class Cursor
+	{
+	public:
+		Cursor();
+		virtual ~Cursor();
+
+		// 更新
+		void update();
+		// 表示
+		void draw();
+
+
+		void setMenuPos(Vec2 pos) { m_menuPos = pos; }
+		void setSize(Vec2 size) { m_size = size; }
+	private:
+		// メニューの表示位置
+		Vec2 m_menuPos;
+		// カーソルのサイズ
+		Vec2 m_size;
+		// メニューの項目数
+		int m_itemNum;
+
+		// 選択している項目の番号
+		int m_selectIndex;
+	};
 
 private:
-
+	// 表示位置(左上座標)
+	Vec2 m_pos;
+	// メニューの選択項目
+	std::vector<Item*> m_pItem;
+	// カーソル
+	Cursor m_cursor;
 };
+
